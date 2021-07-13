67175b6ce013a087ce84d9aba127b87f9f76bd20
Anita Zheng <luo.b.zheng@mail.mcgill.ca>
2021-07-13 13:17:43 -0400

    set debug value

--- lofi_my_time/settings.py
+++ lofi_my_time/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (env('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['lofimytime.herokuapp.com']
 
