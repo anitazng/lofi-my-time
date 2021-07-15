5ff0d21053e3330c60846433251ae8c83c2a0d1e
Anita Zheng <luo.b.zheng@mail.mcgill.ca>
2021-07-15 09:32:53 -0400

    updated staticfiles_storage AGAIN

--- lofi_my_time/settings.py
+++ lofi_my_time/settings.py
@@ -139,8 +139,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
